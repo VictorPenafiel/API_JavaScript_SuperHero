9d2e99e74da8508a8806593ce0060835bc12a740
VictorPenafiel <victorpenafiel@gmail.com>
2024-12-31 14:11:08 -0300

    base

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express();
 const exphbs = require('express-handlebars');
 const port = 3000;
 
+app.use("/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist'));
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
